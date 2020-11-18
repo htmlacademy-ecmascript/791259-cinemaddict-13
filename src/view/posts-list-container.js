eb048d8be1aae978288e0da58a2d7303f9702a02
xena <zizu087@gmail.com>
2020-11-18 12:59:04 -0700

    Cleared Travis errors

--- src/view/posts-list-container.js
+++ src/view/posts-list-container.js
@@ -4,4 +4,4 @@ export const createPostsListContainerTemplate = (sectionClass, headerClass, titl
       <h2 class="films-list__title ${headerClass}">${title}</h2>
       <div class="films-list__container"></div>
   </section>`;
-}
+};
