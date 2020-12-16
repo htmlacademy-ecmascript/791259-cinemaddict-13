6b294fdeffbecbaf8a62a5507d2216e01563d660
xena <zizu087@gmail.com>
2020-12-16 15:40:08 -0700

    added commets and new comment form as components

---
+++ src/view/film-comments.js
@@ -0,0 +1,37 @@
+import {AbstractView} from "./abstract.js";
+
+const createFilmCommentsSection = (comments) => {
+  const createFilmComment = (comment) => {
+
+  	return `<li class="film-details__comment">
+      <span class="film-details__comment-emoji">
+        <img src="./images/emoji/${comment.emotion}.png" width="55" height="55" alt="emoji-${comment.emotion}">
+      </span>
+      <div>
+        <p class="film-details__comment-text">${comment.text}</p>
+        <p class="film-details__comment-info">
+          <span class="film-details__comment-author">${comment.author}</span>
+          <span class="film-details__comment-day">${comment.date}</span>
+          <button class="film-details__comment-delete">Delete</button>
+        </p>
+      </div>
+    </li>`;
+  };
+
+	const commentsList = comments.map((comment) => createFilmComment(comment)).join(``);
+	return `<section class="film-details__comments-wrap">
+    <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments.length}</span></h3>
+    <ul class="film-details__comments-list">${commentsList}</ul>
+  </section>`;
+};
+
+export class FilmCommentsView extends AbstractView {
+	constructor(comments) {
+		super();
+		this._comments = comments;
+	}
+
+	getTemplate() {
+		return createFilmCommentsSection(this._comments);
+	}
+}
